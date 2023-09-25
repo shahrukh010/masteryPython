e96e207e3b6607c4ffb712058c426cb1549660ac
hector <khanshahrukhbsc@gmail.com>
2023-09-25 23:55:56 +0530

    palindrome

--- App.py
+++ App.py
@@ -34,3 +34,44 @@ def remove_duplicates(numbers):
 ids = [1,2,2,3,4,4,5,5];
 result = remove_duplicates(ids);
 print(result);
+
+
+
+#Global variable
+
+count = 0;
+
+def increment():
+    global count;
+    count = count+1;
+    return count;
+
+result = increment();
+print(result);
+
+
+
+#Checking palindrome or not
+
+palindrome_flag = True;
+#inputs = "racecar";
+inputs = "121";
+l = len(inputs);
+
+for index in range(l):
+
+    if inputs[index] !=inputs[l - index -1]:
+       palindrome_flag=False;
+       break;
+    else:
+        palindrome_flag=True;
+
+
+if palindrome_flag:
+    print("Given input is palindrome")
+
+else:
+    print("Given input is not palindrome");
+
+
+
