a5c6cfc6ca53611d1907daa0aef80266bdb664cf
hector <khanshahrukhbsc@gmail.com>
2023-09-30 11:32:06 +0530

    iterate two daimensional array

--- Array.py
+++ Array.py
@@ -35,14 +35,39 @@ print('result:',searchInArray(arr1,6));
 import numpy as np;
 
 twoDarray = np.array([[1,2,3],[4,5,6],[7,8,9]]);
-print(twoDarray);
+#print(twoDarray);
 
 #insert element on column wise
 newTwoDarray = np.insert(twoDarray,0,[[10,20,30]],axis=1);
 
-print(newTwoDarray);
+#print(newTwoDarray);
 
 #inset on row
-
 newRow = np.insert(twoDarray,0,[[50,60,70]],axis=0);
-print(newRow);
+#print(newRow);
+
+#print(newRow[0][1]);
+
+print("access element from two daimensional array");
+
+def accessElement(array,row,col):
+    if row>=len(array) or col>=len(array[0]):
+        print("index is out of bound");
+    else:
+        print(array[row][col]);
+
+accessElement(newRow,0,0);
+
+
+def traverseTwoDarray(array):
+    for row in range(len(array)):
+        for col in range(len(array[0])):
+            print(array[row][col],end=" ");
+        print();
+
+
+nums = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]]);
+
+traverseTwoDarray(nums);
+
+
