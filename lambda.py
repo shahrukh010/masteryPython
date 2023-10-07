45e4d914cb08daf9f0eaa8a0cf952bbc9ed52a25
hector <khanshahrukhbsc@gmail.com>
2023-10-07 22:37:33 +0530

    lambda function

--- lambda.py
+++ lambda.py
@@ -6,3 +6,11 @@ print(result);
 #add
 result = (lambda a,b:a+b)(5,5);
 print(result);
+
+#keyword argumentt to lambda
+result = (lambda a,b:a+b)(b=20,a=33);
+print(result);
+
+#default argument
+result = (lambda a=33,b=16:a-b)();
+print(result);
