2dec3307bd4a87aebdd951a5ddb8f0a0d497ef8d
hector <khanshahrukhbsc@gmail.com>
2023-10-10 20:59:08 +0530

    random algorithm based on nan second

--- random_algo.py
+++ random_algo.py
@@ -1,7 +1,8 @@
 import time
 
 def generate_random_1_digit():
-    current_time = int(time.time())
+    #current_time = int(time.time() * 1000)
+    current_time =int(time.time_ns());
     random_number = (current_time % 49) + 1
     return random_number
 
