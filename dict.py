1f75b6780a6a652b3b334d1892dda3dc6e02c548
hector <khanshahrukhbsc@gmail.com>
2023-10-13 23:02:20 +0530

    updated dictonary function

--- dict.py
+++ dict.py
@@ -45,3 +45,52 @@ delete(data,'price');
 print();
 print();
 print(data);
+
+
+#del data;
+#print(data);
+#data.clear();
+#print(data);
+
+#shallow copy of dictonary.
+data2 = data.copy();
+print(data2);
+import time;
+timestamp = time.time();
+formatted_date = time.strftime("%Y:%m:%d:%H:%M:%S",time.localtime(timestamp));
+
+data2['created'] = formatted_date;
+print(data2);
+print(data);
+
+#create  new dictonary using fromKeys()
+
+new_dict = {}.fromkeys([1,2,3,],0)
+print(new_dict);
+
+#get(,,) if key is presentt then it will return key value else it will return specified value.
+print(data2.get('price',1000));
+
+#item() it is returing list of view tuple pair.
+new_item = data2.items();
+print(new_item);
+
+#it will return view key object.(if data will change then it will reflect also
+new_keys = data2.keys();
+print(new_keys);
+
+
+#popitems() it will pop item from back
+print();
+print(data2);
+#data2.popitem();
+print();
+#print(data2);
+data2.setdefault('price',100);
+created = {'created':time.strftime("%Y:%m:%d:%H:%M:%S",time.localtime(timestamp))};
+print(data);
+#update basied merge two dictionary and make it single /update
+data.update(created);
+print(data);
+
+
