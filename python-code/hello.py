fb351316682e2b6d881a6cc3e971efa6a1820e04
shahrukh <khanshahrukhbsc@gmail.com>
2024-01-16 15:45:12 +0530

    difference way to initialize variable

--- python-code/hello.py
+++ python-code/hello.py
@@ -1,1 +1,10 @@
 print("hello");
+
+#declare multiple variable and initlaize multiple
+a,b,c = 100,200,300;
+print(a,b,c);
+a,b,c=1,1,1
+print(a,b,c);
+#declare and initialize same values
+a=b=c=1;
+print(a,b,c);
