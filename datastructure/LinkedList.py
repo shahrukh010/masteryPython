75c632a34bd765de2a36b54cd0f69181359a28cb
shahrukh <khanshahrukhbsc@gmail.com>
2024-02-14 10:57:05 +0530

    crud operation for singlelist

--- datastructure/LinkedList.py
+++ datastructure/LinkedList.py
@@ -1,57 +1,98 @@
-
 class Node:
+
     def __init__(self,data):
         self.data = data;
         self.nextNode = None;
 
 class List:
-    
+
     def __init__(self):
-        self.head = None;
-        self.first= None;
-        self.tail = None;
         self.size = 0;
+        self.head = None;
+        self.tail = None;
 
-    def add_first(self,value):
-        self.size = self.size+1;
-
-        new_node = Node(value);
-
-        if not self.head:
-            self.head = new_node;
-        else:
-            new_node.nextNode = self.head;
-            self.head = new_node;
 
     def add(self,value):
-        self.size = self.size+1;
+
         new_node = Node(value);
 
-        if not self.first:
-            self.first = new_node;
-            self.tail = self.first;
+        if self.head is None:
+            self.head = new_node;
+            self.tail = self.head;
         else:
-            self.tail.next = new_node;
+            self.tail.nextNode = new_node;
             self.tail = new_node;
 
+
+    def add_first(self,data):
+        new_node = Node(data);
+        
+        if self.head is not None:
+            new_node.nextNode = self.head;
+            self.head = new_node;
+        else:
+            self.head = new_node;
+
+    def add_at(self,data,position):
+        new_node = Node(data);
+        if position == 0:
+            self.add_first(data);
+            return True;
+        else:
+            self.tmp = self.head;
+            for index in range(position-2):
+                self.tmp = self.tmp.nextNode;
+
+            new_node.nextNode = self.tmp.nextNode;
+            self.tmp.nextNode = new_node;
+            return True;
+
+
+    def remove_first(self):
+        
+        current = self.head;
+        current = current.nextNode;
+        self.head = current;
+
+    def remove_at(self,position):
+        if position == 1:
+            self.remove_first();
+        current = self.head;
+
+        for index in range(position-2):
+            current = current.nextNode;
+        current.nextNode = current.nextNode.nextNode;
+
+        
+
     def print_node(self):
+
         current = self.head;
         while current is not None:
             print(current.data);
             current = current.nextNode;
 
 
+    def __str__(self):
+        result_list = [];
+        current = self.head;
+        while current is not None:
+            result_list.append(current.data);
+            current = current.nextNode;
+        result = ','.join(str(num) for num in result_list);
+        return result;
+
 
 list = List();
-#list.add_first(5);
-#list.add_first(4);
-#list.add_first(3);
-#list.add_first(2);
-#list.add_first(1);
-#list.print_node();
-
 list.add(1);
-list.add(2);
 list.add(3);
-list.add(4);
+list.add(5);
+#list.add_first(0);
+list.add_at(2,2);
+list.add_at(4,4);
+list.add(6);
+list.add_at(0,0);
+#list.remove_first();
+list.remove_at(1);
+list.print_node();
 print(list);
